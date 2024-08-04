6a3116627cc4b7e45940c31e6e08e24dfb71c4ed
mohammadhossein <146634977+mohammadhosseinTZ@users.noreply.github.com>
2024-08-04 14:20:44 +0330

    header and footer added

--- js.js
+++ js.js
@@ -8,105 +8,114 @@ let activeHour = pad(hour)[0];
 let activeDay = pad(day)[0];
 
 $(document).ready(function () {
-  updateTimer(".sec" , sec ,activeSec);
-  updateTimer(".min" , min  , activeMin);
-  updateTimer(".hour" , hour , activeHour);
-  updateTimer(".day" , day , activeDay);
+  updateTimer(".sec", sec, activeSec);
+  updateTimer(".min", min, activeMin);
+  updateTimer(".hour", hour, activeHour);
+  updateTimer(".day", day, activeDay);
   $(".mot-timer-wrapper .sec .timer:nth-of-type(2) span").text(pad(sec)[0]);
   $(".mot-timer-wrapper .min .timer:nth-of-type(2) span").text(pad(min)[0]);
   $(".mot-timer-wrapper .hour .timer:nth-of-type(2) span").text(pad(hour)[0]);
   $(".mot-timer-wrapper .day .timer:nth-of-type(2) span").text(pad(day)[0]);
-  $(".iconClose").click(function(){
-
+  $(".iconClose").click(function () {
     $("header").slideUp();
-
-  })
+  });
   function createTimer() {
-  const secInterval = setInterval(() => {
-    if (sec > 0) sec--;
-    else {
-      clearInterval(secInterval);
-      checkMinute();
+    const secInterval = setInterval(() => {
+      if (sec > 0) sec--;
+      else {
+        clearInterval(secInterval);
+        checkMinute();
+      }
+
+      updateTimer(".sec", sec, activeSec);
+    }, 1000);
+  }
+
+  function checkMinute() {
+    if (min > 0) {
+      min--;
+      updateTimer(".min", min, activeMin);
+      sec = 59;
+      createTimer();
+    } else {
+      checkHour();
     }
-
-    updateTimer(".sec" , sec ,activeSec);
-  }, 1000);
-}
-
-function checkMinute() {
-  if (min > 0) {
-    min--;
-  updateTimer(".min" , min , activeMin);
-    sec = 59;
-    createTimer();
-  } else {
-    checkHour();
   }
-}
-function checkHour() {
-  if (hour > 0) {
-    hour--;
-    updateTimer(".hour" , hour , activeHour);
-    min = 59;
-    updateTimer(".min" , min , activeMin);
-    sec = 59;
-    createTimer();
-  } else {
-    checkDay();
+  function checkHour() {
+    if (hour > 0) {
+      hour--;
+      updateTimer(".hour", hour, activeHour);
+      min = 59;
+      updateTimer(".min", min, activeMin);
+      sec = 59;
+      createTimer();
+    } else {
+      checkDay();
+    }
   }
-}
 
-function checkDay() {
-  if (day > 0) {
-    day--;
-    updateTimer(".day" , day , activeDay);
-    hour = 23;
-    updateTimer(".hour" , hour , activeHour);
-    min = 59;
-    updateTimer(".min" , min , activeMin);
-    sec = 59;
-    createTimer();
-  } else {
-    alert("dude its time");
+  function checkDay() {
+    if (day > 0) {
+      day--;
+      updateTimer(".day", day, activeDay);
+      hour = 23;
+      updateTimer(".hour", hour, activeHour);
+      min = 59;
+      updateTimer(".min", min, activeMin);
+      sec = 59;
+      createTimer();
+    } else {
+      alert("dude its time");
+    }
   }
-}
 
-function updateTimer(className , name , prevNum) {
+  function updateTimer(className, name, prevNum) {
+    $(`.mot-timer-wrapper .timer span`).each(function () {
+      $(this).removeClass("active");
+    });
 
-  $(`.mot-timer-wrapper .timer span`).each(function(){
-    $(this).removeClass("active")
-  })
+    if (prevNum !== pad(name)[0]) {
+      name === sec ? (activeSec = pad(name)[0]) : null;
+      name === sec ? (activeHour = pad(name)[0]) : null;
+      name === sec ? (activeMin = pad(name)[0]) : null;
+      name === sec ? (activeDay = pad(name)[0]) : null;
+      $(`.mot-timer-wrapper ${className} .timer:nth-of-type(2) span`).text(
+        pad(name)[0]
+      );
+      $(`.mot-timer-wrapper ${className} .timer:nth-of-type(2) span`).addClass(
+        "active"
+      );
+    }
+    setTimeout(() => {
+      $(`.mot-timer-wrapper ${className} .timer:nth-of-type(1) span`).addClass(
+        "active"
+      );
+    }, 1000);
 
- 
-  if (prevNum !== pad(name)[0]) {
-
-   
-    name === sec ? activeSec = pad(name)[0] :null;
-    name === sec ? activeHour = pad(name)[0] :null;
-    name === sec ? activeMin = pad(name)[0] :null;
-    name === sec ? activeDay = pad(name)[0] :null;
-    $(`.mot-timer-wrapper ${className} .timer:nth-of-type(2) span`).text(pad(name)[0]);
-    $(`.mot-timer-wrapper ${className} .timer:nth-of-type(2) span`).addClass("active");
-
-
+    $(`.mot-timer-wrapper ${className} .timer:nth-of-type(1) span`).text(
+      pad(name)[1]
+    );
   }
-  setTimeout(() => {
-  
-    $(`.mot-timer-wrapper ${className} .timer:nth-of-type(1) span`).addClass("active");
-  }, 1000);
-  
-  $(`.mot-timer-wrapper ${className} .timer:nth-of-type(1) span`).text(pad(name)[1]);
-  
 
-}
-
-
-
-
-createTimer();
+  createTimer();
 });
 
 function pad(num) {
   return num < 10 ? "0" + num.toString() : num.toString();
 }
 
+// footer links
+
+$(document).ready(function () {
+  function checkPosition() {
+    if (window.matchMedia("(max-width:576px)").matches) {
+      $(".list-container").click(function () {
+        $(this)
+          .parent()
+          .find($(".links .wrapper ul li:not(.list-container)"))
+          .slideToggle("slow");
+      });
+    }
+  }
+  checkPosition();
+});
