e4c5137bebfec1dd7f783fd091cb561dafc48041
mohammadhossein <146634977+mohammadhosseinTZ@users.noreply.github.com>
2024-11-16 13:54:55 +0330

    Update js.js

--- js.js
+++ js.js
@@ -1,121 +1,33 @@
-let day = 12;
-let hour = 0;
-let min = 1;
-let sec = 8;
-let activeSec = pad(sec)[0];
-let activeMin = pad(min)[0];
-let activeHour = pad(hour)[0];
-let activeDay = pad(day)[0];
+var countDownDate = new Date($('.discount-banner').attr('data-expiry')).getTime();
+		function timeDown(){
+			var now = new Date().getTime();
+			var distance = countDownDate - now;
+			var day = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var sec = Math.floor((distance % (1000 * 60)) / 1000);
+			$(".mot-timer-wrapper .sec .timer:nth-of-type(2) span").text(pad(sec)[0]);
+			$(".mot-timer-wrapper .min .timer:nth-of-type(2) span").text(pad(min)[0]);
+			$(".mot-timer-wrapper .hour .timer:nth-of-type(2) span").text(pad(hour)[0]);
+			$(".mot-timer-wrapper .day .timer:nth-of-type(2) span").text(pad(day)[0]);
+			$(".mot-timer-wrapper .sec .timer:nth-of-type(1) span").text(pad(sec)[1]);
+			$(".mot-timer-wrapper .min .timer:nth-of-type(1) span").text(pad(min)[1]);
+			$(".mot-timer-wrapper .hour .timer:nth-of-type(1) span").text(pad(hour)[1]);
+			$(".mot-timer-wrapper .day .timer:nth-of-type(1) span").text(pad(day)[1]);
+		}
 
-$(document).ready(function () {
-  updateTimer(".sec", sec, activeSec);
-  updateTimer(".min", min, activeMin);
-  updateTimer(".hour", hour, activeHour);
-  updateTimer(".day", day, activeDay);
-  $(".mot-timer-wrapper .sec .timer:nth-of-type(2) span").text(pad(sec)[0]);
-  $(".mot-timer-wrapper .min .timer:nth-of-type(2) span").text(pad(min)[0]);
-  $(".mot-timer-wrapper .hour .timer:nth-of-type(2) span").text(pad(hour)[0]);
-  $(".mot-timer-wrapper .day .timer:nth-of-type(2) span").text(pad(day)[0]);
-  $(".iconClose").click(function () {
-    $("header").slideUp();
-  });
-  function createTimer() {
-    const secInterval = setInterval(() => {
-      if (sec > 0) sec--;
-      else {
-        clearInterval(secInterval);
-        checkMinute();
-      }
+		$(".iconClose").click(function (e) {
+				e.preventDefault();
+				$(".discount-banner").slideUp();
+		});
 
-      updateTimer(".sec", sec, activeSec);
-    }, 1000);
-  }
+		function pad(num) {
+			return num < 10 ? "0" + num.toString() : num.toString();
+		}
 
-  function checkMinute() {
-    if (min > 0) {
-      min--;
-      updateTimer(".min", min, activeMin);
-      sec = 59;
-      createTimer();
-    } else {
-      checkHour();
-    }
-  }
-  function checkHour() {
-    if (hour > 0) {
-      hour--;
-      updateTimer(".hour", hour, activeHour);
-      min = 59;
-      updateTimer(".min", min, activeMin);
-      sec = 59;
-      createTimer();
-    } else {
-      checkDay();
-    }
-  }
-
-  function checkDay() {
-    if (day > 0) {
-      day--;
-      updateTimer(".day", day, activeDay);
-      hour = 23;
-      updateTimer(".hour", hour, activeHour);
-      min = 59;
-      updateTimer(".min", min, activeMin);
-      sec = 59;
-      createTimer();
-    } else {
-      alert("dude its time");
-    }
-  }
-
-  function updateTimer(className, name, prevNum) {
-    $(`.mot-timer-wrapper .timer span`).each(function () {
-      $(this).removeClass("active");
-    });
-
-    if (prevNum !== pad(name)[0]) {
-      name === sec ? (activeSec = pad(name)[0]) : null;
-      name === sec ? (activeHour = pad(name)[0]) : null;
-      name === sec ? (activeMin = pad(name)[0]) : null;
-      name === sec ? (activeDay = pad(name)[0]) : null;
-      $(`.mot-timer-wrapper ${className} .timer:nth-of-type(2) span`).text(
-        pad(name)[0]
-      );
-      $(`.mot-timer-wrapper ${className} .timer:nth-of-type(2) span`).addClass(
-        "active"
-      );
-    }
-    setTimeout(() => {
-      $(`.mot-timer-wrapper ${className} .timer:nth-of-type(1) span`).addClass(
-        "active"
-      );
-    }, 1000);
-
-    $(`.mot-timer-wrapper ${className} .timer:nth-of-type(1) span`).text(
-      pad(name)[1]
-    );
-  }
-
-  createTimer();
-});
-
-function pad(num) {
-  return num < 10 ? "0" + num.toString() : num.toString();
-}
-
-// footer links
-
-$(document).ready(function () {
-  function checkPosition() {
-    if (window.matchMedia("(max-width:576px)").matches) {
-      $(".list-container").click(function () {
-        $(this)
-          .parent()
-          .find($(".links .wrapper ul li:not(.list-container)"))
-          .slideToggle("slow");
-      });
-    }
-  }
-  checkPosition();
-});
+		$(document).ready(function () {
+			timeDown()
+			const secInterval = setInterval(() => {
+				timeDown()
+			}, 1000);
+		})
